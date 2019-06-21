aea5a39fb8884d322628d755d18211b8dcfe40fb
Mehmet Fatih AKAN <akanmf@yahoo.com>
2019-06-21 14:11:48 +0300

    ilk checkin

---
+++ CoreWebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebApplication1.DataBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        NorthwindContext _northwindContext; 
+        public CustomerController(NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Customer>> List()
+        {            
+            var result= _northwindContext.Customers.ToList();
+            return new ActionResult<List<Customer>>(result);
+        }
+    }
+}
---
+++ CoreWebApplication1/DataBase/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebApplication1.DataBase
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext(DbContextOptions<NorthwindContext> options)
+           : base(options)
+        {
+           
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
